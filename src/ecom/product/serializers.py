f14651e4d15000ce7f1a5e406bf9ab0e6244280d
sanket <sankdhande52@gmail.com>
2022-01-23 21:58:21 +0530

    added product listserializer

--- src/ecom/product/serializers.py
+++ src/ecom/product/serializers.py
@@ -2,11 +2,28 @@ from rest_framework import serializers
 
 from .models import Product
 
-
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(
         max_length=None, allow_empty_file=False, allow_null=True, required=False)
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'image', 'category')
+
+
+
+class ProductList(serializers.ListSerializer):
+    child = ProductListSerializer(many=True)
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'image', 'category')
+
+    def create(self, validated_data):
+        print(validated_data, "-----------------------")
+        books = [Product(**item) for item in validated_data]
+        return self.child.objects.bulk_create(books)
+
+class ProductSerializer(serializers.Serializer):
+    product = ProductList(many=True)
+    # class Meta:
+    #     list_serializer_class = ProductList
--- src/ecom/product/views.py
+++ src/ecom/product/views.py
@@ -1,10 +1,18 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import viewsets, status
 
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,ProductListSerializer
 from .models import Product
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('id')
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
+
+    def create(self, request):
+        data_list = request.data
+        ser = self.serializer_class(data=data_list, many=True)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        return Response(ser.data, status=status.HTTP_201_CREATED)
